286701ef9278c01935cb7d33dd225f664c894e87
Grant <438344441@qq.com>
2018-08-15 15:27:41 +0800

    version1

---
+++ MergeSort/MergeSort.py
@@ -0,0 +1,35 @@
+def MergeSort(alist):
+
+    if len(alist) > 1:
+        mid = len(alist)//2
+        left_list = alist[:mid]
+        right_list = alist[mid:]
+        # recursive calls
+        MergeSort(left_list)
+        MergeSort(right_list)
+
+        # merge array
+        i = 0
+        j = 0
+        k = 0
+        while i < len(left_list) and j < len(right_list):
+            if left_list[i] <= right_list[j]:
+                alist[k] = left_list[i]
+                i = i + 1
+            elif left_list[i] > right_list[j]:
+                alist[k] = right_list[j]
+                j = j + 1
+            k = k + 1
+        while i == len(left_list) and j < len(right_list):
+            alist[k] = right_list[j]
+            j = j + 1
+            k = k + 1
+        while i < len(left_list) and j == len(right_list):
+            alist[k] = left_list[i]
+            i = i + 1
+            k = k + 1
+
+
+alist = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+MergeSort(alist)
+print(alist)
