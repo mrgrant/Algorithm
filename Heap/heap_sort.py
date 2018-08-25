4f2c484b257448c047be3e8c41c1e25162e2054a
Grant <mrgrantcy@gmail.com>
2018-08-25 16:09:48 +0800

    Heap Sort ver 2

--- Heap/heap_sort.py
+++ Heap/heap_sort.py
@@ -1,48 +1,39 @@
-def parent(i):
-    return i//2
-
-
-def left(i):
-    return 2*i+1
-
-
-def right(i):
-    return 2*i+2
-
-
-def max_heapify(A, i):
-    l = left(i)
-    r = right(i)
-    if l <= len(A)-1 and A[l] > A[i]:
+def max_heapify(A, n, i):
+    """
+    max_heapify
+    :param A: array A
+    :param n: size of a heap
+    :param i: root at index i
+    :return:
+    """
+    l = 2*i+1
+    r = 2*i+2
+    if l < n and A[l] > A[i]:
         largest = l
     else:
         largest = i
-    if r <= len(A)-1 and A[r] > A[largest]:
+    if r < n and A[r] > A[largest]:
         largest = r
     if not largest == i:
         A[i], A[largest] = A[largest], A[i]
-        max_heapify(A, largest)
+        max_heapify(A, n, largest)
 
 
 def build_max_heap(A):
     for i in range(len(A)//2, -1, -1):
-        max_heapify(A, i)
+        max_heapify(A, len(A), i)
 
 
 def heap_sort(A):
     build_max_heap(A)
-    for i in range(len(A)-1, 1, -1):
+    for i in range(len(A)-1, 0, -1):
         A[0], A[i] = A[i], A[0]
-        temp = A.pop()
-        ret.append(temp)
-        max_heapify(A, 0)
+        max_heapify(A, i, 0)
 
 
 if __name__ == '__main__':
-    A = [2, 5, 1, 6, 3]
-    ret = []
+    arr = [12, 11, 13, 5, 6, 7]
+    heap_sort(arr)
+    print(arr)
 
-    heap_sort(A)
-    ret += A
-    print(ret)
 
