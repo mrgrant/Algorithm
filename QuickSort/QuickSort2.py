069ba814ebd765110f2f8a80ae2dcd65750e3826
Grant <mrgrantcy@gmail.com>
2018-09-04 14:49:35 +0800

    QuickSort 9.2

--- QuickSort/QuickSort2.py
+++ QuickSort/QuickSort2.py
@@ -4,7 +4,9 @@ import random
 
 
 def partition(array, l, r):
-    pivot_idx = random.randint(l, r)
+    # pivot_idx = random.randint(l, r)
+    global cnt
+    pivot_idx = l + (r-l)//2
     p = array[pivot_idx]
     # swap the random pivot to the head of array
     array[l], array[pivot_idx] = array[pivot_idx], array[l]
@@ -12,6 +14,7 @@ def partition(array, l, r):
     i = l + 1
     for j in range(l+1, r+1):
         # swap the number less than pivot at left of the array
+        cnt += 1
         if array[j] < p:
             array[j], array[i] = array[i], array[j]
             i = i + 1
@@ -31,7 +34,18 @@ def quicksort(array, start=0, end=None):
     quicksort(array, i+1, end)
 
 
+def load_data(filename):
+    data = []
+    with open(filename, 'r') as f:
+        for i in f.readlines():
+            data.append(int(i))
+    return data
+
+
 if __name__ == "__main__":
-    alist = [8, 2, 4, 5, 3]
+    alist = load_data('QuickSort.txt')
+    # alist = [1, 6, 8, 10, 7 ,5 ,2, 9, 4, 3]
+    cnt = 0
     quicksort(alist)
+    print(cnt)
     print(alist)
